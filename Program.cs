0e0e4421ee17fd62b984cee31162f0247ffc66cc
GustavoHEngelmann <gustavo_engelmann@estudante.sesisenai.org.br>
2025-11-18 15:57:53 -0300

    Initialize ASP.NET Core application with MySQL
    é pra connection string

---
+++ Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+var app = builder.Build();
+
+app.MapControllers();
+app.Run();
